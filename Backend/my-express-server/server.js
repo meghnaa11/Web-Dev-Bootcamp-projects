f8914c5ffd7d2b077694de422103bbd04e97649d
Meghna <67739559+meghnaa11@users.noreply.github.com>
2023-07-16 12:04:45 +0530

    Add files via upload

---
+++ Backend/my-express-server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("<h1>Hello</h1>");
+})
+
+app.get("/contact", function(req, res) {
+    res.send("Contact me at xyz@abc.com");
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
